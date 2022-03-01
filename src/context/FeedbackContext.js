9dc7dcf28c60a2f8b5ef9cde37087957d4e628c1
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-03-01 08:07:49 -0800

    Update FeedbackContext.js

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -8,17 +8,17 @@ export const FeedbackProvider = ({ children }) => {
     {
       id: 1,
       rating: 10,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+      text: 'Feedback item 1.',
     },
     {
       id: 2,
       rating: 9,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+      text: 'Feedback item 2.',
     },
     {
       id: 3,
       rating: 8,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+      text: 'Feedback item 3.',
     },
   ]);
 
